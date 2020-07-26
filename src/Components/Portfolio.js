7224f11665e7757df50164151dcb8512ca5cae8f
Janice <jopanice@gmail.com>
2020-07-26 15:51:50 -0700

    Updated projects header

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -66,7 +66,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Projects</h1>
+            <h1>Projects I'm Involved In</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
